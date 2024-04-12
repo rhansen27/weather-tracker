6849446bd6ef32b1fa32e8a79e8bb1059ae6baa6
rph <rhansen0127@gmail.com>
2024-04-11 22:07:36 -0500

    saving current progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,38 @@
 // variable declaration
-const searchButton = document.getElementById('search');
-const searchInput = document.getElementById('city');
-const form = document.getElementById('serach-form');
-
+const searchButton = document.getElementById("search");
+const searchInput = document.getElementById("city");
+const form = document.getElementById("serach-form");
+const container = document.querySelector("main");
 // API key
 const API_KEY = "305a69ec163b25e41504278d241e096d";
 
-function getWeather(city){
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
-        .catch(error => console.log(error));
+function getWeather(city) {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+  fetch(url).then(function (response) {
+    console.log(response);
+    if (!response.ok) {
+      errorMessage(error);
+    }
+  });
 }
 
-function handleSubmit(e){
-    e.preventDefault();
-    const city = searchInput.value;
-    getWeather(city);
+function errorMessage(error) {
+  const alert = document.createElement("div");
+  alert.classList.add(
+    "alert",
+    "alert-danger",
+    "alert-dismissible",
+    "fade",
+    "show"
+  );
+  alert.innerText = error;
+  container.appendChild(alert);
 }
 
-window.addEventListener('submit', handleSubmit);
+function handleSubmit(e) {
+  e.preventDefault();
+  const city = searchInput.value;
+  getWeather(city);
+}
+
+window.addEventListener("submit", handleSubmit);
